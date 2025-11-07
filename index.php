2949efea4b49d9837eaf3d5e8e9ebf69c09f71c3
valdirsillva <valdirpiresba@gmail.com>
2025-11-07 18:44:44 -0300

    implementa para centralizar qual controller chamar

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+use App\Controllers\DasbboardController;
+use App\Controllers\MovimentacaoController;
 use App\Controllers\ProdutoController;
 
-$controller = new ProdutoController();
-$controller->index();
+/**
+ * Captura a ação da URL ou define o padrão
+ * 
+ */
+$acao = $_GET['acao'] ?? 'dashboard';
+
+// 🔹 Decide qual controller chamar com base na ação
+switch ($acao) {
+  case 'dashboard':
+  default:
+    (new DasbboardController())->index();
+    break;
+  case 'movimentacoes':
+    (new MovimentacaoController())->index();
+    break;
+  case 'produtos':
+    (new ProdutoController())->index();
+    break;
+}
